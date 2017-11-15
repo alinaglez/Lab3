9bcba9c6eae6797c510ea0e7a4fed4c42cd58f56
4010e4 <4010e4@anon.ece.uprm.edu>
2017-11-15 10:43:22 -0400

    Changed the Jframe title and its screen position to the center.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("ALINA");
+		JFrame myFrame = new JFrame("Alina");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(917, 373);
 		myFrame.setSize(200, 200);
