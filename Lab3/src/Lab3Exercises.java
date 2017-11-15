faaa0d4da19df93b234cbef1a14a12eb51098825
4010e4 <4010e4@anon.ece.uprm.edu>
2017-11-15 10:50:31 -0400

    Change Jframe title to "Alina S. Gonzalez"

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,9 +2,9 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Alina");
+		JFrame myFrame = new JFrame("Alina S. Gonzalez");
 		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(917, 373);
+		myFrame.setLocation(918, 373);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
